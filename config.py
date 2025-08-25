4f9ad0f86cca9a801773a5ab066fd0099c240798
tiwariji03ok-jpg <tiwariji03ok@gmail.com>
2025-08-25 14:22:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ ADMINS = int(os.environ.get("ADMINS", "7832892326"))
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://tiwariji03ok:w8HbrYoZHSromyyY@cluster0.yx6r3sd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "tiwariji03ok")
+DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
